cea7f8589fc58646e064a94e995684ee0231ee7a
Nicolas <nicolas10morschel@gmail.com>
2023-05-08 13:21:08 -0300

    commit

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended : true}));
 app.use(express.json());
 
 const router = express.Router();
@@ -8,12 +8,24 @@ app.use('/', router.get('/', (req, res, next) => {
     res.status(200).send("<h1>API - CHAT<h1>");
 }));
 
-app.use("/", router.get("/sobre", (req, res) => {
+app.use("/sobre",router.get("/sobre", (req, res, next) => {
     res.status(200).send({
-        "nome": "API - CHAT",
-        "versão": "0.1.0",
-        "autor": "nicolas morschel"
+        "nome":"API - CHAT",
+        "versão":"0.1.0",
+        "autor":"nicolas morschel"
     });
 }));
 
-module.exports = app;
+app.use("/salas",router.get("/salas", async(req, res, next) => {
+    const salaController = require("./controllers/salaController");
+    let resp = await salaController.get();
+    res.status(200).send(resp);
+}));
+
+app.use('/',router.post('/entrar',async(req, res, next)=>{
+    const usuarioController = require('.controller/usuarioController');
+    let resp = await usuarioController.entrar(req.body.nick);
+    res.status(200).send(resp);
+}));
+
+module.exports=app;
